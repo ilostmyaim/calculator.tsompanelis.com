56f018b3022e6773e023acd15e6e61221893b2e9
ilostmyaim <devilsfullmoon@gmail.com>
2022-09-21 19:23:15 +0300

    Added invertClickHandler function

--- src/App.js
+++ src/App.js
@@ -84,6 +84,15 @@ const App = () => {
     }
   };
 
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: "",
+    });
+  };
+
 
   return (
     <Wrapper>
